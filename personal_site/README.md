780363fdc3279fc26f4d625bef35e395933bc257
Stewart Valdez <valdezstewart@gmail.com>
2023-04-06 13:54:35 -0700

    Added a folder to clean up the look.

---
+++ personal_site/README.md
@@ -0,0 +1,6 @@
+# personal-website
+Personal Website designed to be my Software Engineering portfolio.
+The start of this portfolio will be a brief description of my background.
+The idea of this portfolio is to add some style while keeping it simple.
+Some information and ideas are inspired from digging into W3 Schools and MDN.
+Used lorem for a text placeholder.
---
+++ personal_site/main.js
@@ -0,0 +1,25 @@
+// Below is a functio to create time and date clock.
+function updateDateTime() {
+    const currentDatetime = new Date();
+    const formattedDatetime = currentDatetime.toLocaleString();
+    document.getElementById('current-datetime').innerText = formattedDatetime;
+  }
+  
+  // Update the date and time when the page loads
+  updateDateTime();
+  
+  // Update the date and time every second
+  setInterval(updateDateTime, 1000);
+
+function updateDateTime() {
+    const currentDatetime = new Date();
+    const formattedDatetime = currentDatetime.toLocaleString();
+    document.getElementById('current-datetime').innerText = formattedDatetime;
+  }
+  
+  // Update the date and time when the page loads
+  updateDateTime();
+  
+  // Update the date and time every second
+  setInterval(updateDateTime, 1000);
+
